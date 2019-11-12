05f0aa636adc51458d0de87bef4aedd9659179d6
royyan1207070 <royyan120707@gmail.com>
2019-11-12 10:53:44 +0700

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import { Text, View } from 'react-native'
+import MenJa from './component/MenJa'
 
 export default class App extends Component{
   render(){
     return(
-      <View>
-        <Text>halo</Text>
-      </View>
+      <MenJa/>
     )
   }
 }
